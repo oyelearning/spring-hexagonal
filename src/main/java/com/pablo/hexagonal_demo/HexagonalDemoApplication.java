1c66ee3723dff9be455f6cd4f384a180fe4bca9a
pablopena <pablopena@4set.es>
2024-06-24 12:52:44 +0200

    add base project

---
+++ src/main/java/com/pablo/hexagonal_demo/HexagonalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.hexagonal_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/hexagonal_demo/HexagonalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.hexagonal_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
